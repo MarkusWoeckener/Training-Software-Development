d88159c660b80256244d8407da58c1d42fb7d92b
MarkusWoeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-07-01 23:03:55 +0200

    and another algorithm task

---
+++ Ausbildung/PHP/Algorithm_practice/4_range_check.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Ask user for an integer. If the integer is within a range of +-10 around 100
+and 200 print "True".
+*/
+
+# Ask user for input
+$number = readline("Enter a number: ");
+
+# Check if $number is within one of the given ranges
+if 90 <= $number <= 110 || 190 <= $number <= 210 {
+    echo "True";
+} else {
+    echo "False";
+}
