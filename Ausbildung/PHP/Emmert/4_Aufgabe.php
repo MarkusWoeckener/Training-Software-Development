351bff7dc325b36f36b1d8757ebc3558e49e120b
Markus Woeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-07-16 09:06:35 +0200

    Algorithm practice

---
+++ Ausbildung/PHP/Emmert/4_Aufgabe.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Erstellen Sie ein Programm, welches den Nutzer auffordert eine Zahl einzugeben.
+Wird keine 1 oder 2 eingetippt, erscheint eine Fehlermeldung. Ist die Eingabe
+hingegeben richtig, erscheint entweder die Ausgabe „Sie haben 1 gewählt“ oder
+entsprechend „Sie haben 2 gewählt“.
+Mit fußsteuerter Schleife.
+
+Pweudocode:
+*/
