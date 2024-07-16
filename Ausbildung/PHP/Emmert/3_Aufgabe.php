f2b6a00f973d47f61414927f694604a0c721bc46
Markus Woeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-07-16 13:37:30 +0200

    Algorithm practice

--- Ausbildung/PHP/Emmert/3_Aufgabe.php
+++ Ausbildung/PHP/Emmert/3_Aufgabe.php
@@ -7,4 +7,41 @@ entsprechend „Sie haben 2 gewählt“.
 Mit kopfgesteuerter Schleife.
 
 Pseudocode:
+int zahl = 0
+
+Wiederhole solange zahl != 1 && zahl != 2
+    Ausgabe: "Wählen Sie Option 1 oder 2 aus: "
+    Eingabe: zahl
+    Wenn zahlt != 1 && zahl != 2
+    Ausgabe: "Ungültige Eingabe"        
+    Ende Wenn
+Ende Wiederhole
+
+Fallunterscheidung
+    zahl == 1
+        Ausgabe: "Sie haben 1 gewählt"
+    zahl == 2
+        Ausgabe: "Sie haben 2 gewählt"
+Ende Fallunterscheidung
+Ende
 */
+
+zahl = 0;
+
+while (zahl != 1 && zahl !=2){
+    echo "Wählen Sie Option 1 oder 2 aus: ";
+    $zahl = intval(trim(fgets(STDIN)));
+    if ($zahl != 1 && $zahl != 2) {
+        echo "Ungültige Eingabe\n";
+    }
+}
+
+switch (zahl) {
+    case 1:
+        echo "Sie haben 1 gewählt\n";
+        break;
+    case 2:
+        echo "Sie haben 2 gewählt\n";
+        break;
+}
+?>
--- Ausbildung/PHP/Emmert/4_Aufgabe.php
+++ Ausbildung/PHP/Emmert/4_Aufgabe.php
@@ -7,4 +7,42 @@ entsprechend „Sie haben 2 gewählt“.
 Mit fußsteuerter Schleife.
 
 Pweudocode:
+Anfang
+int zahl = 0
+Wiederhole
+    Ausgabe: "Wählen Sie Option 1 oder 2 aus: "
+    Eingabe: zahl
+    Wenn zahl != 1 && zahl != 2
+        Ausgabe: "Ungüültige Eingabe"
+    Ende Wenn
+solange zahl != 1 && zahl != 2
+Ende Wiederhole
+Fallunterscheidung
+    zahl == 1
+        Ausgabe: "Sie haben 1 gewählt"
+    zahl == 2
+        Ausgabe: "Sie haben 2 gewählt"
+Ende Fallunterscheidung
+Ende
 */
+
+$zahl = 0;
+
+do{
+    echo "Wählen Sie Option 1 oder 2 aus: ";
+    $zahl = intval(trim(fgets(STDIN)));
+    if ($zahl != 1 && $zahl != 2) {
+        echo "Ungültige Eingabe\n";
+    }
+} while ($zahl != 1 && $zahl != 2);
+
+switch ($zahl) {
+    case 1:
+        echo "Sie haben 1 gewählt\n";
+        break;
+    case 2:
+        echo "Sie haben 2 gewählt\n";
+        break;
+}
+?>
+?>
