0750c1338ea962bb7429df6d3917d7b3ba922cc4
MarkusWoeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-07-24 23:42:19 +0200

    Created a Java test project

---
+++ Eigenes/Java/Test_Project/TestProject/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = getName();
+        System.out.println("Hello, World! Hello " + name);
+    }
+
+    //ask user for their name
+    public static String getName() {
+        String name;
+        System.out.print("Enter your Name: ");
+        name = System.console().readLine();
+        return name;
+    }
+}
