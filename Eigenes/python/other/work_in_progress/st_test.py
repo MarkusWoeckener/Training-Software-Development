3fb632487aa684c3b8099d61b2f0618e52a0e9ef
Markus Woeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-08-14 09:45:05 +0000

    done: input form, save data as CSV and simple output as table

--- Eigenes/python/other/work_in_progress/st_test.py
+++ Eigenes/python/other/work_in_progress/st_test.py
@@ -1,13 +1,39 @@
 import streamlit as st
 import datetime
+import pandas as pd
+import os
 
 st.title("Ein- Ausgaben Test")
 st.subheader("Buchung eingeben")
 
 datum:  datetime.date = st.date_input("Datum")
-typ = st.selectbox("Art der Buchung", ("Einkommen", "Ausgabe"))
-kategorie = st.selectbox("Kategorie", ("Lebensmittel", "Tanken", "Getränke", "Sonstiges"))
-betrag = st.number_input("Betrag")
+typ: "str" = st.selectbox("Art der Buchung", ("Einkommen", "Ausgabe"))
+kategorie: "str" = st.selectbox("Kategorie", ("Lebensmittel", "Tanken", "Getränke", "Sonstiges"))
+betrag: float = st.number_input("Betrag")
 
-if st.button("Submit"):
-    st.write(f"Hello {name}! You are {age} years old.")
+def buchung_speichern(datum, typ, kategorie, betrag):
+    data = {
+        "Datum": [datum],
+        "Art": [typ],
+        "Kategorie": [kategorie],
+        "Betrag": [betrag]
+    }
+    df = pd.DataFrame(data)
+    
+    if not os.path.exists("buchungen.csv"):
+        df.to_csv("buchungen.csv", index=False)
+    else:
+        df.to_csv("buchungen.csv", mode="a", header=False, index=False)
+
+if st.button("Abschicken"):
+    if (typ == "Ausgabe" and betrag > 0):
+        betrag = -betrag
+    buchung_speichern(datum, typ, kategorie, betrag)
+    st.success("Erfolgreich gespeichert")
+
+if st.button("Buchungen anzeigen"):
+    if os.path.exists("buchungen.csv"):
+        display  = pd.read_csv("buchungen.csv")
+        st.dataframe(display)
+    else:
+        st.warning("Keine Buchung vorhanden")
