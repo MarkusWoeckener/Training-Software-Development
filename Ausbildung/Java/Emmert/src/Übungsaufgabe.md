5603ae978ac0f8a157cd6991b149d5aaac89cc4f
Markus Woeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-10-08 14:44:30 +0200

    more practice...

---
+++ Ausbildung/Java/Emmert/src/Übungsaufgabe.md
@@ -0,0 +1,16 @@
+# Übungsaufgabe
+Bitte schreiben Sie ein Programm in Pseudocode das den Benutzer auffordert eine Zahlt einzugeben. Es prüft ob die Zahl gerade oder ungerade ist und gibt eine entsprechende Nachricht aus.
+
+Pseudocode:
+```
+Anfang
+int eingabe
+Ausgabe: "Geben sie eine Zahl ein: "
+Eingabe: zahl
+Wenn Modulo (zahl) == 0, dann
+    Ausgabe: "Die Zahl ist gerade"
+sonst
+    Ausgabe: "Die Zahl ist ungerade"
+Ende Wenn
+Ende
+```
---
+++ Eigenes/Java/Test_Project/TestProject/src/Zahlenschloss_3.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Zahlenschloss_3 {
+    public static void main(String[] args) throws Exception{
+        int[] kombination = {1, 7, 11}; //Gesuchte Kombination
+        int versuche = 3; //Anzahl der erlaubten Versuche
+
+        for (int i = 0; i < versuche; i++) {
+            if (schloss(kombination)) {
+                System.out.println("Richtige Kombination");
+                break;
+            }
+            else {
+                System.out.println("Falsche Kombination");
+            }
+        }
+
+    }
+    
+    private static boolean schloss(int[] kombination) throws Exception {
+        int zahl;
+        boolean gleich = true;
+
+        //Für jede Stelle der Kombination eine Eingabe des Benutzers anfordern
+        //und prüfen. Ggf. Flag für falsche Eingabe setzen
+        for (int i = 0; i < kombination.length; i++) {
+            System.out.print("Geben Sie eine Zahl der Kombination ein: ");
+            try {
+                zahl = getInt();
+            }
+            catch (NumberFormatException e) {
+                gleich = false;
+                continue;
+            }
+
+            if (zahl != kombination[i]) { //Prüfen der Eingabe
+                gleich = false; //Flag setzen
+            }
+        }
+        return gleich; //Flag zurückgeben, ob richtige Kombination
+    }
+
+    static int getInt() throws Exception {
+		//Gibt ein Integer zurück
+		BufferedReader tastatur = new BufferedReader(new InputStreamReader(System.in));
+		
+		return Integer.parseInt(tastatur.readLine());
+	}
+}
