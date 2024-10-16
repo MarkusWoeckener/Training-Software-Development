18bf9b34da89907c0ad7ff65a5870e75be996beb
Markus Woeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-10-16 12:58:35 +0200

    continuing with first OOP project

--- Ausbildung/eclipse/Gebrauchtwagenhändler/src/bfw/oop/Auto.java
+++ Ausbildung/eclipse/Gebrauchtwagenhändler/src/bfw/oop/Auto.java
@@ -4,10 +4,42 @@ public class Auto {
 	/**Diese Klasse stellt einen Gebrauchtwagen dar. Sie enthält
 	 * den Preis, das Modell, das Baujahr, den Kilometerstand
 	 * und die Farbe.*/
+	
+	//Attribute
 	private double preis; //Preis des Autos
 	private String modell; //Modell des Autos
 	private int baujahr; //Baujahr des Autos
 	private int kilometerstand; //Kilometerstand des Autos
 	private String farbe; //Farbe des Autos
-
+	
+	//Konstruktor
+	public Auto (double wert1, String wert2, int wert3, int wert4, String wert5) {
+		this.preis = wert1;
+		this.modell = wert2;
+		this.baujahr = wert3;
+		this.kilometerstand = wert4;
+		this.farbe = wert5;
+	}
+	
+	//Getter Methoden
+	public double getPreis() {
+		return preis;
+	}
+	public String getModell() {
+		return modell;
+	}
+	public int getBaujahr() {
+		return baujahr;
+	}
+	public int getKilometerstand() {
+		return kilometerstand;
+	}
+	public String getFarbe() {
+		return farbe;
+	}
+	
+	//Setter Methoden
+	public void setPreis(double wert) {
+		this.preis = wert;
+	}
 }
--- Ausbildung/eclipse/Gebrauchtwagenhändler/src/bfw/oop/Gebrauchtwagen.java
+++ Ausbildung/eclipse/Gebrauchtwagenhändler/src/bfw/oop/Gebrauchtwagen.java
@@ -2,6 +2,11 @@ package bfw.oop;
 
 public class Gebrauchtwagen {
 	public static void main(String[] args) {
-		
+		Auto auto1 = new Auto(12343.54, "VW Friedolin", 1962, 158328, "beige");
+		System.out.println("Modell: " + auto1.getModell());
+		System.out.println("Baujahr: " + auto1.getBaujahr());
+		System.out.println("Preis: " + auto1.getPreis());			
+		auto1.setPreis(8786.34);
+		System.out.println("Neuer Preis: " + auto1.getPreis());
 	}
 }
