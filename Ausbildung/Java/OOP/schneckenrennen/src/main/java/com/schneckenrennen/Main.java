8231a1a88b482271a6f9d7e5a7cc737fdccce33c
Markus Woeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2024-12-03 14:48:10 +0100

    refining comments

--- Ausbildung/Java/OOP/schneckenrennen/src/main/java/com/schneckenrennen/Main.java
+++ Ausbildung/Java/OOP/schneckenrennen/src/main/java/com/schneckenrennen/Main.java
@@ -10,17 +10,29 @@ public class Main {
 	 * @param args {@code String[]} Befehlszeilenargumente
 	 */
 	public static void main(String[] args) throws Exception{
+		//Hauptmenü anzeigen
 		int auswahl = Gui.mainMenu();
+		//Soll ein Rennen erstellt, oder das Beispiel gestartet werden?
 		Rennen rennen = Gui.menuAuswahl(auswahl);
+		//Wenn ein neues Rennen erstellt wurde
 		if (rennen != null) {
+			//Schnecken hinzufügen
 			Gui.addRennschnecke(rennen);
+			//Soll ein Wettbüro erstellt werden?
 			Wettbuero wb = Gui.wettbuero(rennen);
+			//Wenn ja, die Wetten annehmen
 			if (wb != null) Ui.wettAnnahme(wb, rennen);
-			//ToDo Rennen durchführen
+			//Rennen durchführen
 			startRennen(rennen, wb);
 		}
 	}
 
+	/**
+	 * Startet ein Rennen<br>
+	 * Je nach Auswahl des Benutzers mit oder ohne Wettbüro
+	 * @param rennen {@code Rennen} das erstellte Rennen
+	 * @param wb {@code Wettbuero} das Wetbüro, wenn erstellt
+	 */
 	static void startRennen(Rennen rennen, Wettbuero wb) {
 		if (wb != null) wb.rennenDurchfuehren(); //Output needs further tweaking!
 		else {
