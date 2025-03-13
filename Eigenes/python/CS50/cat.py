7266656362eb191d4551baec515540d51c9b7e57
MarkusWoeckener <118171745+MarkusWoeckener@users.noreply.github.com>
2025-03-13 15:36:43 +0100

    updating comments

--- Eigenes/python/CS50/cat.py
+++ Eigenes/python/CS50/cat.py
@@ -3,12 +3,14 @@ def main():
     miau(number)
 
 def getNumber():
+    """Asks user to input a number n and returns it if it is positive."""
     while True:
         n = int(input("n:"))
         if n > 0:
             return n
 
 def miau(n):
+    """Prints 'miau' n times."""
     for _ in range(n):
         print("miau")
 
